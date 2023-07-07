9147838385b1f146205d4a392d610b0f7f24130e
MeetMRP <meetpatelmrp1005@gmail.com>
2023-07-07 20:19:44 +0530

    filterd the scraped data

--- Utility/loadDbUtility.py
+++ Utility/loadDbUtility.py
@@ -1,11 +1,31 @@
 from dbModel import Session, Enquiry
 from sqlalchemy import exists
 from bs4 import BeautifulSoup
+from datetime import timedelta, datetime
 
 
 async def chechNumber(number):
     number = number.replace(' ', '').replace('+91', '')
     return number if len(number) == 10 else False
+
+
+async def setData(data):
+    last_call = data[9]
+
+    if last_call != '-':
+        next_call = data[10]
+        follow_up = data[13]
+        dateFormat = '%d-%b-%Y'
+        if follow_up == '1':
+            next_call = (datetime.strptime(last_call, dateFormat) + timedelta(days=3)).strftime(dateFormat)
+            data[10] = next_call
+        elif follow_up == '2':
+            next_call = (datetime.strptime(last_call, dateFormat) + timedelta(days=7)).strftime(dateFormat)
+            data[10] = next_call
+
+    return data
+    
+
 
 
 async def loadData(response):
@@ -20,6 +40,12 @@ async def loadData(response):
         data[1] = await chechNumber(data[1])
         if not data[1]:
             continue
+
+        next_call = data[10]
+        follow_up = data[13]
+        if next_call == '-' and follow_up != '3':
+            data = await setData(data)
+
         q = session.query(exists().where(Enquiry.Contact == data[1])).subquery()
         if not session.query(q).scalar():
             session.add(Enquiry(*data))
